f6f975b6ebbe718bb2e8ad738d7d7076b178608f
onlyandri <onlyandri97@gmail.com>
2021-04-25 10:36:36 +0700

    mengubah configurasi dasar

---
+++ application/views/ustadzah/dashboard.php
@@ -0,0 +1,256 @@
+<div class="main-panel">
+	<div class="content">
+		
+		<div class="panel-header bg-primary-gradient">
+			<div class="page-inner py-5">
+				<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+					<div>
+						<h2 class="text-white pb-2 fw-bold uppercase">Dashboard Guru</h2>
+						<h5 class="text-white op-7 mb-2">Hallo Muhammad Agni Setiaawan,Ahlan wa Sahlan <br>
+						</h5>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="page-inner mt--5">
+			<div class="row mt--2">
+				<div class="col-md-4">
+					<div class="card card-secondary">
+						<div class="card-body skew-shadow">
+							<h1>5</h1>
+							<div class="pull-right">
+								<h3 class="fw-bold op-8">Siswa Tahfidz Anda</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-dark bg-secondary-gradient">
+						<div class="card-body bubble-shadow">
+							<h1>2</h1>
+							<div class="pull-right">
+								<h3 class="fw-bold op-8">Kelompok Tahfidz Anda</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-dark bg-secondary2">
+						<div class="card-body curves-shadow">
+							<h1>3</h1>
+							<div class="pull-right">
+								<h3 class="fw-bold op-8">Jumlah Setoran Masuk</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Row Card No Padding -->
+			<div class="row row-card-no-pd">
+				<div class="col-sm-6 col-md-3">
+					<div class="card card-stats card-round">
+						<div class="card-body ">
+							<div class="row">
+								<div class="col-5">
+									<div class="icon-big text-center">
+										<i class="flaticon-chart-pie text-warning"></i>
+									</div>
+								</div>
+								<div class="col-7 col-stats">
+									<div class="numbers">
+										<p class="card-category">Setoran Hafalan Masuk</p>
+										<h4 class="card-title">3</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="card card-stats card-round">
+						<div class="card-body ">
+							<div class="row">
+								<div class="col-5">
+									<div class="icon-big text-center">
+										<i class="flaticon-check text-success"></i>
+									</div>
+								</div>
+								<div class="col-7 col-stats">
+									<div class="numbers">
+										<p class="card-category">Setoran Sudah Dinilai</p>
+										<h4 class="card-title">3</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="card card-stats card-round">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-5">
+									<div class="icon-big text-center">
+										<i class="flaticon-error text-danger"></i>
+									</div>
+								</div>
+								<div class="col-7 col-stats">
+									<div class="numbers">
+										<p class="card-category">Setoran Belum Dinilai</p>
+										<h4 class="card-title">0</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="card card-stats card-round">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-5">
+									<div class="icon-big text-center">
+										<i class="flaticon-round text-primary"></i>
+									</div>
+								</div>
+								<div class="col-7 col-stats">
+									<div class="numbers">
+										<p class="card-category">Setoran Belum Dikoreksi</p>
+										<h4 class="card-title">1</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-12">
+					<h1>Akses Menu</h1>
+				</div>
+				<div class="col-md-6">
+					<div class="card card-info card-annoucement card-round">
+						<div class="card-body text-center">
+							<div class="card-opening">Lihat Setoran Siswa</div>
+							<div class="card-desc">
+								Klik Menu Ini Untuk Melihat dan Menilai setoran hafalan yang disetorkan
+								oleh siswa
+							</div>
+							<div class="card-detail">
+								<a href="">
+									<div class="btn btn-light btn-rounded">Lihat Setoran Siswa</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="card card-info card-annoucement card-round">
+						<div class="card-body text-center">
+							<div class="card-opening">Buat Quiz Hafalan</div>
+							<div class="card-desc">
+								Klik Menu Ini Untuk Menginput Quiz Hafalan Untuk Siswa
+							</div>
+							<div class="card-detail">
+								<a href="">
+									<div class="btn btn-light btn-rounded">Buat Quiz Hafalan</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-6">
+					<div class="card card-info card-annoucement card-round">
+						<div class="card-body text-center">
+							<div class="card-opening">Edit Profile</div>
+							<div class="card-desc">
+								Klik Menu Ini Untuk Mengedit Profile Anda seperti mengganti email, nomor telepon dan foto
+							</div>
+							<div class="card-detail">
+								<a href="">
+									<div class="btn btn-light btn-rounded">Edit Profile</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-6">
+					<div class="card card-info card-annoucement card-round">
+						<div class="card-body text-center">
+							<div class="card-opening">Hubungi Admin</div>
+							<div class="card-desc">
+								Klik Menu Ini Jika Anda Membutuhkan Bantuan Teknis seperti reset password, dan lain sebagainya
+							</div>
+							<div class="card-detail">
+								<a href="">
+									<div class="btn btn-light btn-rounded">Hubungi Admin</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<div class="card full-height">
+						<div class="card-header">
+							<div class="card-title">Aktivitas Terbaru Dari Kelompok Anda</div>
+							<a href="">Untuk Melihat Semua Setoran Terbaru Klik Disini</a></h4><br><br>
+						</div>
+						<div class="card-body">
+							<ol class="activity-feed">
+								<h4  data-aos="zoom-out">
+									<li data-aos="zoom-out" class="feed-item                                                                                                                  feed-item-danger
+									">
+									<time class="date" datetime="9-25">2021-01-22 18:25:32</time>
+									<span class="text">Razky Febriansyah Menyetorkan <a
+										href="">"Al-Fatihah : 1 Al-Fatihah : 1"</a></span><br>
+										<div class="badge 
+										badge-success
+										">Sudah Dinilai</div><br>
+									</li>
+
+									<li data-aos="zoom-out" class="feed-item 
+									feed-item-success
+									feed-item-secondary
+									feed-item-danger
+									">
+									<time class="date" datetime="9-25">2021-01-21 15:42:35</time>
+									<span class="text">Henry Augusta Menyetorkan <a
+										href="">"Al-Fatihah : 1 Al-Fatihah : 1"</a></span><br>
+										<div class="badge 
+										badge-success
+										">Sudah Dinilai</div><br>
+									</li>
+
+									<li data-aos="zoom-out" class="feed-item 
+									">
+									<time class="date" datetime="9-25">2021-01-21 03:53:48</time>
+									<span class="text">Razky Febriansyah Menyetorkan <a
+										href="">"Al-Fatihah : 2 Al-Fatihah : 2"</a></span><br>
+										<div class="badge 
+										badge-success
+										">Sudah Dinilai</div><br>
+									</li>
+
+								</ol>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+
+</div>
+
+</div>
